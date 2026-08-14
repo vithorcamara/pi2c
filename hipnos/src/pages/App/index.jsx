4e5e311d4b048c5de1216fef1c8b732ee81b5078
Vitor Câmara <107072024+vithorcamara@users.noreply.github.com>
2026-08-14 18:38:41 +0000

    core: finish template

--- hipnos/src/pages/App/index.jsx
+++ hipnos/src/pages/App/index.jsx
@@ -3,6 +3,7 @@ import reactLogo from '../../assets/react.svg'
 import appLogo from '/favicon.svg'
 import PWABadge from '../../components/PWABadge/index.jsx'
 import './index.css'
+import { MdWhatsapp } from "react-icons/md";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <h1>Hello World!</h1>
+      <MdWhatsapp />
       <PWABadge />
     </>
   )
